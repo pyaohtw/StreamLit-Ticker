517d86611a2b6d2204884d7191671c8a08df9aae
hydrobot004 <91904299+hydrobot004@users.noreply.github.com>
2022-12-27 22:37:44 -0500

    Update simp_stock-goog.py

--- simp_stock-goog.py
+++ simp_stock-goog.py
@@ -13,7 +13,7 @@ Shown are the stock **closing price** and **volume** of Apple!
 #define the ticker symbol
 tickerSymbol = 'AAPL'
 #get data on this ticker
-tickerData = yf.Ticker(tickerSymbol)
+tickerData = yf.Ticker(AAPL)
 #get the historical prices for this ticker
 tickerDf = tickerData.history(period='1d', start='2010-5-31', end='2020-5-31')
 # Open	High	Low	Close	Volume	Dividends	Stock Splits
